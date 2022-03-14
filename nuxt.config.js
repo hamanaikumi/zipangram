c5198dde16787a0ce037bac5f8cd3e1038df3e71
jurasora1010 <ikumi.hamana@rakus-partners.co.jp>
2022-03-14 09:28:53 +0900

    SPAに変更（比較のため）

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
   css: [],
 
   // vercelデプロイ用
-  ssr: true,
+  ssr: false,
   target: 'server',
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
